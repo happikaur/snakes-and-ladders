8489aabdc50b7e7fd1fea5b1ab08de1024770d6d
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-01-31 20:56:22 +0000

    Fixed the positioning issue of the boxes. Changed the numbers order for the board. Added background and font for each box. Made  2 players.

---
+++ app.js
@@ -0,0 +1,11 @@
+// Boxes number need to flow and not break
+// Person need to choose the avatar
+// Enter the name
+// choose who rolls first
+// Role dice
+  // Random number from 1 to 6
+// Moves box by that number
+// If in the box with ladder go up to the box number where ladder ends
+// If in the box with snake go down to the box number where snake ends
+
+
